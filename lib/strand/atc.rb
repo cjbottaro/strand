05caa224876991756537a76f5c7a8e34f0e33bd8
Christopher J. Bottaro <cjbottaro@alumni.cs.utexas.edu>
2011-08-24 02:41:39 -0400

    Atc and ConditionVariable

---
+++ lib/strand/atc.rb
@@ -0,0 +1,41 @@
+class Strand
+  class Atc #:nodoc:
+    
+    def initialize(options = {})
+      @timeout  = options[:timeout]
+      @cond     = ConditionVariable.new
+      @states   = []
+    end
+
+    # Wait for state to happen.
+    def wait(state, timeout = nil)
+      return true if @states.include?(state)
+      timeout ||= @timeout
+      if timeout
+        wait_with_timeout(state, timeout)
+      else
+        wait_without_timeout(state)
+      end
+    end
+
+    def signal(state)
+      @states << state
+      @cond.signal
+    end
+
+  private
+    
+    def wait_with_timeout(state, timeout)
+      while not @states.include?(state)
+        return @states.include?(state) if @cond.wait(timeout) == false
+      end
+      true
+    end
+
+    def wait_without_timeout(state)
+      @cond.wait while not @states.include?(state)
+      true
+    end
+
+  end
+end
